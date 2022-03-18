6b5f91fc0b0699fe552037b15f466105bea71c60
Viviana Gomez <vivianakgp@gmail.com>
2022-03-17 21:43:26 -0600

    SearchBox new functionality without seach button

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,34 +1,28 @@
 import React from "react";
 import axios from 'axios';
 import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-   // https://rickandmortyapi.com/api/character/?name=rick&status=alive
-// https://rickandmortyapi.com/api/location/${value}
 const SearchBox = ({dataSet})=> {
     const [value, setValue] = useState('');
     const [locations, setLocations] = useState([]);
     const [showDropDown, setShowDropDown] = useState(false);
 
-    const getLocation = () => axios.get(`https://rickandmortyapi.com/api/location/?name=${value}`);
-    
     const cleanDropDown = () => {
         setShowDropDown(false);
         setValue('');
     };
-    const updateNewData = () => {
-        getLocation().then(res => {
+    const updateNewData = (locName) => {
+        axios.get(`https://rickandmortyapi.com/api/location/?name=${locName}`).then(res => {
             // console.log(res.data.results[0])
             // see example of response: https://rickandmortyapi.com/api/location/?name=Earth%20(C-137)
             dataSet(res.data.results[0]);
         })
         .then(()=> cleanDropDown())
         .catch(err => console.log(err));
-        
     };
-    const filterLocationsByName = () => {
-        getLocation().then(res => {
+    const filterLocationsByName = (ev) => {
+        setValue(ev.target.value);
+        axios.get(`https://rickandmortyapi.com/api/location/?name=${value}`).then(res => {
             // console.log(res.data.results)
             // res.data.results--> get an array(results) with many objs
             setLocations(res.data.results)
@@ -43,20 +37,14 @@ const SearchBox = ({dataSet})=> {
                     type="text" 
                     value={value} 
                     placeholder="Location Name"
-                    onChange={ e => {
-                        setValue(e.target.value);
-                        filterLocationsByName();
-                    }}
-                
-                />
-
-                <button onClick={updateNewData}>{<FontAwesomeIcon icon={faMagnifyingGlass} />}</button>
+                    onChange={ e => {e.target.value !== ''?filterLocationsByName(e):cleanDropDown()}}
+                /> 
             </div>
             <div className="drop-down">
                 <li style={{display: showDropDown? "block":"none", padding:showDropDown? "5%":"0"}}>
                     {
                         locations.map(elem => (
-                        <ul  key={elem.id} onClick={()=> setValue(elem.name)}>
+                        <ul  key={elem.id} onClick={()=> updateNewData(elem.name)}>
                             {elem.name}
                         </ul>
                         ))
